c3a73f4d54e9948dc05c14be962a93c7ed473f6d
J <j@Jacobs-MacBook-Pro.local>
2021-01-31 02:27:12 -0800

    added log for user to check, and cleaned up codes a bit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
-// Set up the game variables 
+// Set up the game variables and other initial values
 let growthCount = 0
 let hungerCount = 0
 let time = 0
 let evolved = 0
 let gameover = false
 let sleeping = false
-
 let randGrowth = 0
 let randFeed = 0
 let randTrainCost = 0
-
+const modal = document.getElementById("modal")
 const growth = document.querySelector("#growth")
 const hunger = document.getElementById("hunger")
 const sMessage = document.getElementById("someMessage")
@@ -20,43 +19,20 @@ const action = document.getElementById("action")
 const growthBar = document.getElementById("growthBar")
 const hungerBar = document.getElementById("hungerBar")
 const trainButton = document.getElementById("train")
-
+let logLastLog = document.getElementById("last-log")
 let lastLog = document.createElement("p")
 lastLog.style.fontFamily = "'Londrina Solid', cursive;"
 lastLog.style.fontSize = "3vw"
 lastLog.style.textAlign = "center"
+lastLog.style.color = "white"
 
-// const checking = () => {
-//     const timer = setInterval (() => {
-//         if (gameover !== true){
-//             if (hungerCount < 0) {
-//                 hungerCount = 0
-//                 hunger = hungerCount + "%"
-//                 hungerBar.style.width = hungerCount + "%"
-//             } else if (growth > 100) {
-//                 growthCount = 100
-//                 growth = growthCount + "%"
-//                 growthBar.style.width = growthCount + "%"
-//             }
-//         }
-//         else {
-//             clearInterval(timer)
-//         }
-//     }, 10)
-// }
 // Making action img random
 const randImg = (numbOfImg) => {
     let randNumber = Math.floor((Math.random()* numbOfImg) + 1)
     return randNumber
-    // if (whichToChange == "train"){
-    //     thePet.src = "training" + whichImg + ".gif"
-    // } else if (whichToChange == "feed") {
-    //     thePet.src = "eating" + whichImg + ".gif"
-    // }
 }
 
 // switching img L & R
-// let compare = thePet.src.split("/").pop()
 const switchImg = () => {
     const timer = setInterval(() => {
         if (gameover == true) {
@@ -101,6 +77,7 @@ const petHungriness = () => {
             hungerCount = 100
             mainMessage.innerText = "GAME OVER"
             mainMessage.style.opacity = 1
+            logLastLog.style.opacity = 0
             document.body.appendChild(lastLog)
             sMessage.innerText = "Reached Max Hunger"
             sMessage.style.color = "red"
@@ -143,14 +120,14 @@ const petHungriness = () => {
     }, 500)}
 
 // setTimer takes two required and an optional parameters
-// theButton is for the button that it is using with, chooseTime for desired time duration, and sleepornot, type in "yes" for third parameter when using with sleep function
-// add method so Train turns to evolve/ascend when reaching 100
+// theButton is for the button that it is using with, chooseTime for desired time duration, and sleepornot, type in "yes" in third parameter when using with sleep function
+// add method so Train button text turns to evolve/ascend when reached 100
 const setTimer = (theButton, chooseTime, sleepornot) => {
     let tempTime = chooseTime
     const timer = setInterval(() => {
         document.querySelector(theButton).classList.add("disabled")
-
         // end of sleep timer
+        // sleep function disabled all buttons but all timers keep running while hunger meter is stopped by +0 per set duration with check sleeping in petHungriness function
         if(sleepornot == "yes" && tempTime <= 0) {
             sleeping = false
             // hide sleeping pet 
@@ -232,9 +209,7 @@ const feedPet = document.getElementById("feed").addEventListener("click", (event
     // }
 })
 
-// for the random growth to add in train button and log in ImgTimer
-
-
+// change growth font color depending on thresholds
 const changeGrowthColor = () => {
     if (growthCount >= 100) {
         document.getElementById("growth").style.color = "blue"
@@ -253,7 +228,7 @@ const changeGrowthColor = () => {
     }
 }
 // Train pet button
-// It will not be availble if hungriness is over a certain amount, else increase growth valuse & hungriness with train
+// Should not be availble if hunger is over 70, else increase growth valuse & hunger with train function
 const trainPet = document.getElementById("train").addEventListener("click", (event) => {
     event.preventDefault()
     randGrowth = Math.floor((Math.random()*100)/2)
@@ -262,6 +237,7 @@ const trainPet = document.getElementById("train").addEventListener("click", (eve
         alert("Cannot Train/Evolve/Ascend when hunger is above 70")
     } else if (hungerCount < 70 && growthCount >= 100 && evolved == 1) {
         console.log("You won!")
+        logLastLog.style.opacity = 0
         document.querySelector("section").style.display = "none"
         mainMessage.innerText = "You Won!"
         mainMessage.style.opacity = 1
@@ -275,7 +251,6 @@ const trainPet = document.getElementById("train").addEventListener("click", (eve
         action.style.opacity = 1
         document.getElementById("restart").style.opacity = 1
     } else if (hungerCount < 70 && growthCount >= 100) {
-// problem: switch img to fast evolve img not showing on time
         thePet.style.opacity = 0
         imgTimer(2, "evolving")
         // swtich img to second
@@ -325,17 +300,12 @@ const trainPet = document.getElementById("train").addEventListener("click", (eve
         changeGrowthColor()
         setTimer("#train", 5)
         imgTimer(3, "training")
-    } else {
-        if (gameover !== true) {
-        alert("Cannot Train/Evolve/Ascend when hunger is above 70")
-        }
-        
     }
     growth.innerText = "Growth: " + growthCount + "%"
     growthBar.style.width = growthCount + "%"
-    
 })
 
+// start sleep button timer, but real function runs in setTimer & petHungriness function
 const letSleep = document.getElementById("sleep").addEventListener("click", (event) => {
     event.preventDefault()
     setTimer("#sleep", 5, "yes")
@@ -365,6 +335,7 @@ const resetButtons = () => {
 
 const imgTimer = (time, theAction) => {
     let tempTime = time
+    thePet.style.opacity = 0
     const timer = setInterval(() => {
         if (tempTime > 0 && action.style.opacity == 1) {
         } else if (sleeping == true) {
@@ -376,49 +347,60 @@ const imgTimer = (time, theAction) => {
                 if (randFeed > 30) {
                     sMessage.innerText = "Hunger - " + randFeed + "%" + "   Lucky!"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else if (randFeed < 10) {
                     sMessage.innerText = "Hunger - " + randFeed + "%" + "   Unlucky!"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else {
                     sMessage.innerText = "Hunger - " + randFeed + "%"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 }
             } else if (theAction == "training") {
                 if (randGrowth == randTrainCost) {
                     sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Fair trade"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else if (randGrowth - randTrainCost > 30) {
                     sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Super lucky!"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else if (randGrowth - randTrainCost > 20) {
                     sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Not bad!"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else if (randGrowth - randTrainCost > 10) {
                     sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Great!"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else if (randTrainCost - randGrowth > 30) {
                     sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Too unlucky!"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else if (randTrainCost - randGrowth > 20) {
                     sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Hmmm.."
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else if (randTrainCost - randGrowth > 10) {
-                    sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Some gains"
+                    sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Ok..."
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 } else {
                     sMessage.innerText = "Growth + " + randGrowth + "% & Hunger + " + randTrainCost + "%" + "   Making progress"
                     sMessage.style.opacity = 1
-                    lastLog.innerText = "Last action: " + sMessage.innerText
+                    lastLog.innerText = "Last log: " + sMessage.innerText
+                    logLastLog.innerText = lastLog.innerText
                 }
             }
         } else {
@@ -430,50 +412,12 @@ const imgTimer = (time, theAction) => {
         tempTime--
 }, 1000)}
 
-
-// Starting
-const modal = document.getElementById("modal")
-// const confirm = document.getElementById("start")
 modal.style.display = "block"
 form.style.display = "none"
 thePet.style.opacity = 0
 switchImg();
-
-// const firstPet = document.getElementById("first")
-// const secondPet = document.getElementById("second")
-// const thirdPet = document.getElementById("third")
-
-// firstPet.addEventListener("click", function() {
-//   modal.style.display = "none"
-//   thePet.style.opacity = 1
-//   form.style.display = "flex"
-//   petHungriness();
-// })
-
-// secondPet.addEventListener("click", function() {
-//     if (thePet.src.split("/").pop() == "1r.gif") {
-//         thePet.src = "assets/2r.gif"
-//     } else if (thePet.src.split("/").pop() == "1l.gif") {
-//         thePet.src = "assets/2l.gif"
-//     }
-//     modal.style.display = "none";
-//     thePet.style.opacity = 1
-//     form.style.display = "flex"
-//     petHungriness();
-// })
-
-// thirdPet.addEventListener("click", function() {
-//     if (thePet.src.split("/").pop() == "1r.gif") {
-//         thePet.src = "assets/3r.gif"
-//     } else if (thePet.src.split("/").pop() == "1l.gif") {
-//         thePet.src = "assets/3l.gif"
-//     }
-//     modal.style.display = "none";
-//     thePet.style.opacity = 1
-//     form.style.display = "flex"
-//     petHungriness();
-// })
-
+// Starting
+// for pet selection preview at the start
 const preview = document.getElementById("preview")
 const petSelection = document.getElementById("petSelection")
 petSelection.addEventListener("mouseover", function(e) {
@@ -488,7 +432,6 @@ petSelection.addEventListener("mouseover", function(e) {
         preview.style.opacity = 1
     }
 })
-
 
 petSelection.addEventListener("click", function(e) {
     if (e.target.innerText == "#1") {
@@ -510,5 +453,5 @@ petSelection.addEventListener("click", function(e) {
     thePet.style.opacity = 1
     form.style.display = "flex"
     petHungriness()
-    // checking()
+    document.getElementById("last-log").style.opacity = 1
 })
